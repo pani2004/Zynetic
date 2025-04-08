68c49bedf2b749facfb9a19a23211dd24b2dd575
pani2004 <debashispani61@gmail.com>
2025-04-09 03:21:22 +0530

    Revert "updated package files for deploying to render"
    This reverts commit f676201fe01ca90bc1b293f372efb34e01153169.

--- server/index.js
+++ server/index.js
@@ -1,47 +1,31 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import path from 'path';
 import cookieParser from 'cookie-parser';
-import connectDB from './db/index.js';
+import connectDB from "./db/index.js";
 import userRoutes from './routes/user.route.js';
 import productRoutes from './routes/product.route.js';
 import { notFound, errorHandler } from './middlewares/error.middleware.js';
-import { fileURLToPath } from 'url';
-
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 dotenv.config();
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(notFound);
-app.use(errorHandler);
 
 connectDB()
-  .then(() => {
-    const PORT = process.env.PORT || 8000;
-
-    app.listen(PORT, () => {
-      console.log(`✅ Server is running on port ${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.log('❌ MongoDB connection failed:', err);
-  });
-
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port :${process.env.PORT}`)
+    })
+})
+.catch((err)=>[
+    console.log("MongoDB connection failed",err)
+])
 
 app.use('/api/auth', userRoutes);
 app.use('/api/products', productRoutes);
 
+app.use(notFound);
+app.use(errorHandler);
 
-const frontendPath = path.join(__dirname, 'public');
-app.use(express.static(frontendPath));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(frontendPath, 'index.html'));
-});
-
-
+const PORT = process.env.PORT || 5000;
