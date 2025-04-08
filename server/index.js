e4d7c139773ce6a874ca5f9c7c7656a695a536c0
pani2004 <debashispani61@gmail.com>
2025-04-09 01:33:49 +0530

    initialize server with routes, middleware, and environment configuration

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import connectDB from "./db/index.js";
+import userRoutes from './routes/user.route.js';
+import productRoutes from './routes/product.route.js';
+import { notFound, errorHandler } from './middlewares/error.middleware.js';
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port :${process.env.PORT}`)
+    })
+})
+.catch((err)=>[
+    console.log("MongoDB connection failed",err)
+])
+
+app.use('/api/auth', userRoutes);
+app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ server/routes/user.route.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+} from '../controllers/user.controller.js';
+
+const router = express.Router();
+
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.post('/logout', logoutUser);
+router.get('/refresh-token', refreshAccessToken);
+
+export default router;
